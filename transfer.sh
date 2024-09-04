99939b1dc8d4b73b77447408c49c80ebf17d120f
azegas <azegaspa@gmail.com>
2024-09-04 15:58:36 +0300

    ignore more things

--- transfer.sh
+++ transfer.sh
@@ -7,7 +7,15 @@ REMOTE_PATH="/home/arvypi/GIT/invest"
 
 # Transfer files using rsync
 echo "Transferring files..."
-rsync -av --delete --exclude='.git' --exclude='__pycache__' ./ "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}"
+rsync -av --delete \
+    --exclude='.git' \
+    --exclude='__pycache__' \
+    --exclude='.env' \
+    --exclude='app.log' \
+    --exclude='data' \
+    --exclude='static/images' \
+    ./ "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}"
+
 if [ $? -ne 0 ]; then
     echo "Failed to transfer files."
     exit 1

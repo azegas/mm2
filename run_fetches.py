d4e9f60343e983575109df618f24eca1a8fdfdf8
azegas <azegaspa@gmail.com>
2024-09-06 08:43:10 +0300

    run all fetches function

--- run_fetches.py
+++ run_fetches.py
@@ -2,7 +2,11 @@ from fetch_stock_data import fetch_stock_data
 from fetch_historical_data import fetch_historical_data
 from draw_charts import generate_stock_chart_high_prices_with_volume
 
-if __name__ == "__main__":
+def run_all_fetches():
     fetch_stock_data()
     fetch_historical_data()
     generate_stock_chart_high_prices_with_volume()
+
+
+if __name__ == "__main__":
+    run_all_fetches()

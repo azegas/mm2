f5c4a10e5c87c963d6838f66cc5c8ddec7c2c0a2
azegas <azegaspa@gmail.com>
2024-09-06 17:05:49 +0300

    clean comments

--- app.py
+++ app.py
@@ -7,34 +7,26 @@ from dotenv import load_dotenv
 load_dotenv()
 base_dir = os.getenv("BASE_DIR")
 
-# Initialize a Flask application
 app = Flask(__name__)
 
 
-# Route for the home page, serves the index.html file from the "static" directory
 @app.route("/")
 def home():
     return send_from_directory("static", "index.html")
 
 
-# Route to read stock data from a JSON file and return it as a JSON response
 @app.route("/read_stock_data_from_file")
 def stock_data():
     file_path = os.path.join(base_dir, "data/stock_data.json")
     if os.path.exists(file_path):
-        # Open the JSON file and load its contents
         with open(file_path, "r") as file:
-            data = json.load(file)  # Parse the JSON data
-
-        # Return the parsed data as a JSON response
+            data = json.load(file)
         return jsonify(data)
     else:
-        # If the file doesn't exist, return an error message with a 404 status
         return jsonify({"error": "Data not found"}), 404
 
 
-# Run the Flask application only if this script is executed directly
 if __name__ == "__main__":
     app.run(
         debug=True
-    )  # Start the Flask development server with debug mode enabled
+    )
